b6cc8cbad3328d9db592134b67c08879af761906
Gabriela Pérez <gabzozz@gmail.com>
2021-04-28 23:45:30 -0300

    primer commit

--- EnClase/app.js
+++ EnClase/app.js
@@ -1,48 +1,4 @@
-// Redondear un numero mas bajo
-console.log (Math.floor (45.89));
 
-//redondear hacia arriba
-console.log (Math.round (45.52));
-
-//Obtener el num maximo de una lista de numeros
-
-console.log (`El número máximo es: ${Math.max (56,5,45,90,5,1000)}`);
-
-//Obtener el minimo
-
-console.log (`El número mínimo es: ${Math.min (56,5,45,90,5,1000)}`);
-
-
-//Obtener el número entero
-let numero= 67.876533
-console.log (numero);
-numero =parseInt (numero);
-let decimal = "456.322345"
-console.log (parseFloat (decimal));
-
-
-//Elevar a la potencia
-let base= numero
-let exponente= 3
-
-console.log (`El numero ${base} elevado a la potencia ${exponente} es de ${Math.pow (base, exponente)} `)
-
-//Calcular raiz cuadrado
-console.log (Math.sqrt (64));
-
-//Numero aleatorio
-
-console.log (Math.round (Math.random ()* 100)); //math.round seria para redondear, math.random para que salga un n° aleatorio
-
-//Obtener numero con 2 decimales
-
-decimal= parseFloat (decimal)
-
-console.log (Math.round (decimal *100) /100);
-
-//ToFixed
-
-let decimalString = parseFloat 
 
 
 
