1c25f79c2dea70397ad5e2ec270ec1d19bf9bce4
Gabriela Pérez <gabzozz@gmail.com>
2021-04-28 23:18:54 -0300

    primer commit

---
+++ TAREAS-JS/tarea28-04.js
@@ -0,0 +1,13 @@
+
+let numero1=25;
+let numero2=50;
+
+let suma= numero1 + numero2;
+let resta= numero1 - numero2;
+let multiplicacion= numero1*numero2;
+let división= numero1/numero2;
+
+console.log(numero1 == numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 != numero2);
+
