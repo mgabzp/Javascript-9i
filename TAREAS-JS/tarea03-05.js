0cfa0b317c37c87e16f518ee5bbaf5510b951d22
Gabriela Pérez <gabzozz@gmail.com>
2021-05-03 00:24:29 -0300

    Tarea 03-05-21

---
+++ TAREAS-JS/tarea03-05.js
@@ -0,0 +1,19 @@
+//Tarea 1- Capitalizar un nombre
+
+let nombre= "gabriela"
+
+console.log (nombre.toUpperCase ());
+
+//Tarea 2
+
+let numeroA= 10;
+let numeroB= 5;
+let numeroC= 30;
+
+
+console.log (`El número máximo es: ${Math.max (numeroA, numeroB, numeroC)}`);
+
+console.log (`El número mínimo es: ${Math.min (numeroA, numeroB, numeroC)}`);
+
+console.log (Math.sqrt (numeroB));
+
