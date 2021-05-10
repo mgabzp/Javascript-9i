2dea0ba0e6aeb3c647544eb88cfd76fcbf59ddc5
Gabriela Pérez <gabzozz@gmail.com>
2021-05-10 20:32:19 -0300

    Modificaciones desafío 1

--- TAREAS-JS/tarea10-05-des1.js
+++ TAREAS-JS/tarea10-05-des1.js
@@ -1,13 +1,13 @@
 // Desafío 1
 
-let palabra = String (prompt ('Ingresar una palabra, para salir presione cancelar'));
+let palabra = prompt ('Ingresar una palabra, para salir presione cancelar'));
 
 let cadenas=[]
 
 if (palabra == null){
     console.error ('El dato ingresado no es un texto');
 } else {
-while (palabra !== null){
+while (palabra){
         cadenas.push (palabra)
         palabra = prompt ('Ingresar una palabra, para salir presione cancelar');
     }
